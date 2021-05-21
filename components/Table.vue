623f32c7f58202574794494197047c155bc45a99
agnes <agnes@indochat.co.id>
2021-05-21 15:43:12 +0800

    feat: 調整 table 組件

---
+++ components/Table.vue
@@ -0,0 +1,47 @@
+<template>
+  <table class="table-list container mx-auto px-6 py-8">
+    <thead>
+      <TableHead :columns="columns">
+        <input
+          class="w-6/12 rounded-full px-4 py-2 border-2 border-cool-gray-3 outline-none focus:border-green-500 transition-all duration-150"
+          type="text" name="search" id="search"
+          placeholder="Search"
+        />
+      </TableHead>
+    </thead>
+    <tbody class="bg-white">
+      <template v-for="(datas, key) in listDatas">
+        <tr v-for="(data, idx) in datas" :key="idx">
+          <td
+            class="px-6 py-4 whitespace-nowrap border-b border-gray-200"
+            v-for="col in columns" :key="col.name">
+            <template v-if="key === col.name">
+              <p class="text-gray-500">
+                {{ data.value }}
+              </p>
+            </template>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap border-b border-gray-200">
+            <button class="px-3 py-1 bg-green-600 text-white rounded-md text-sm hover:bg-green-700 focus:outline-none transition-colors duration-150">查看</button>
+          </td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import TableHead from '~/components/TableHead.vue'
+
+@Component({
+  components: {
+    TableHead
+  },
+})
+export default class TableList extends Vue {
+  @Prop({ type: Array, required: true, default: () => { return [] } })
+  columns!: Array<any>
+  @Prop({ type: Object, required: true, default: () => { return {} } })
+  listDatas!: {}
+}
+</script>
--- pages/database/list/index.vue
+++ pages/database/list/index.vue
@@ -1,50 +1,23 @@
 <template>
   <div class="container mx-auto px-6 py-8">
     <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
-      <table class="table-list container mx-auto px-6 py-8">
-        <tbody class="bg-white">
-          <TableHead :columns="columns">
-            <input
-              class="w-6/12 rounded-full px-4 py-2 border-2 border-cool-gray-3 outline-none focus:border-green-500 transition-all duration-150"
-              type="text" name="search" id="search"
-              placeholder="Search"
-            />
-          </TableHead>
-          <template v-for="(datas, key) in listDatas">
-            <tr v-for="(data, idx) in datas" :key="idx">
-              <td
-                class="px-6 py-4 whitespace-nowrap border-b border-gray-200"
-                v-for="col in columns" :key="col.name">
-                <template v-if="key === col.name">
-                  <p class="text-gray-500">
-                    {{ data.value }}
-                  </p>
-                </template>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap border-b border-gray-200">
-                <button class="px-3 py-1 bg-green-600 text-white rounded-md text-sm hover:bg-green-700 focus:outline-none transition-colors duration-150">查看</button>
-              </td>
-            </tr>
-          </template>
-        </tbody>
-      </table>
+      <Table :columns="columns" :listDatas="listDatas" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-import TableHead from '~/components/TableHead.vue'
+import Table from '~/components/Table.vue'
 
 @Component({
   layout: 'Dashboard',
   components: {
-    TableHead
+    Table
   },
 })
 
 export default class DashboardListPage extends Vue {
-
   columns: Array<any> = [
     {
       name: 'table_name',
@@ -62,12 +35,11 @@ export default class DashboardListPage extends Vue {
       },
     ]
   }
-
 }
 </script>
 
 <style scoped>
-table.table-list tbody tr > *:last-child {
+::v-deep table.table-list tr > *:last-child {
   @apply text-right;
 }
 </style>
