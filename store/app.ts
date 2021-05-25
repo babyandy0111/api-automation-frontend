12b4d265496f17a98b5f352b8280dafee9d1f0ad
Candice Kuo <killua7812@gmail.com>
2021-05-25 12:03:12 +0800

    feat: 修改列表名稱

--- store/app.ts
+++ store/app.ts
@@ -10,17 +10,17 @@ export default class App extends VuexModule {
   public authenticated: boolean = false
   public sidebar: Array<any> = [
     {
-      label: '資料庫管理',
+      label: 'API 管理',
       icon: 'database',
       name: 'database',
       subMenu: [
         {
           label: '建立數據來源',
           name: 'database-create',
-          // url: '/database/create',
+          url: '/database/create',
         },
         {
-          label: '資料庫列表',
+          label: 'API 列表',
           name: 'database-list',
           url: '/database/list',
         }
@@ -30,13 +30,25 @@ export default class App extends VuexModule {
       icon: 'code',
       name: 'api',
       subMenu: [
+        // {
+        //   label: 'API 列表',
+        //   name: 'api-list',
+        //   // url: '/api/list',
+        // },
         {
-          label: 'API 列表',
-          name: 'api-list',
-          // url: '/api/list',
-        },
+          label: '建立 API',
+          name: 'api-create',
+          // url: '/api/create',
+        }
+        // 未來可能會新增
+      ]
+    },{
+      label: '域名管理',
+      icon: 'code',
+      name: 'domain',
+      subMenu: [
         {
-          label: '建立 API 列表',
+          label: '建立 API',
           name: 'api-create',
           // url: '/api/create',
         }
